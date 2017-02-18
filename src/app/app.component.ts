39be9e5d3f9164f9c9e04d5c060047f3e294647f
Haksun Kim <itpro@haksunkim.com>
2017-02-18 14:30:29 +1000

    Set title
    - set title using TitleService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  blogTitle: string = "Haksun Kim";
+
+  constructor (private titleService: Title) {
+    titleService.setTitle(this.blogTitle);
+  }
+}
