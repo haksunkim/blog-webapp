2eba7ea3ac92edd25b1e05e47d2af26e02aa45a9
Haksun Kim <itpro@haksunkim.com>
2017-02-19 14:53:33 +1000

    Added footer
    - added footer component and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,8 @@ import { StaticContent } from "./model/staticContent.model";
 
 @Component({
   selector: 'my-app',
-  template: `<div class="container-fluid">
-    <app-header></app-header>
-  </div>
-  <div class="container-fluid" style="background-color:#efefef;">
-    <div class="container" style="background-color:#ffffff;">
-      <div class="row">
-        <div class="col-12 col-lg-9">
-          <router-outlet></router-outlet>
-        </div>
-        <div class="col-12 col-lg-3">
-          <app-sidebar></app-sidebar>
-        </div>
-      </div>
-    </div>
-  </div>`,
+  moduleId: module.id,
+  templateUrl: "app.component.html",
 })
 export class AppComponent  {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { BlogModule } from "./blog/blog.module";
 import { AppComponent }  from './app.component';
 import { HeaderComponent } from "./header.component";
 import { SidebarComponent } from "./sidebar.component";
+import { FooterComponent } from "./footer.component";
 import { routing } from "./app.routing";
 
 @NgModule({
   imports:      [ BrowserModule, ModelModule, routing, BlogModule ],
-  declarations: [ AppComponent, HeaderComponent, SidebarComponent ],
-  bootstrap:    [ AppComponent, HeaderComponent, SidebarComponent ]
+  declarations: [ AppComponent, HeaderComponent, SidebarComponent, FooterComponent ],
+  bootstrap:    [ AppComponent, HeaderComponent, SidebarComponent, FooterComponent ]
 })
 export class AppModule { }
