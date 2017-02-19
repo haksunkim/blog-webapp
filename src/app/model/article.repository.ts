f9b80e348e62e3ff43b9e0f3803fb551df7742d9
Haksun Kim <itpro@haksunkim.com>
2017-02-19 23:42:13 +1000

    Tag sidebar implemented
    - tag sidebar added

--- src/app/model/article.repository.ts
+++ src/app/model/article.repository.ts
@@ -5,9 +5,25 @@ import { RestDataSource } from "./rest.datasource";
 @Injectable()
 export class ArticleRepository {
   private articles: Article[] = [];
+  private tags: string[] = [];
 
   constructor(private datasource: RestDataSource) {
-    datasource.getArticles().subscribe(data => this.articles = data);
+    datasource.getArticles().subscribe(data => {
+      this.articles = data;
+      let fullTags: string[] = [];
+
+      for (let article of this.articles) {
+        for (let tag of article.tags) {
+          fullTags.push(tag);
+        }
+      }
+      
+      for (let tag of fullTags) {
+        if (this.tags.indexOf(tag) == -1) {
+          this.tags.push(tag);
+        }
+      }
+    });
   }
 
   getArticles(tag: string = null) : Article[] {
@@ -16,5 +32,9 @@ export class ArticleRepository {
 
   getArticle(id: number) : Article {
     return this.articles.find(a => a.id == id);
+  }
+
+  getTags() : string[] {
+    return this.tags;
   }
 }
